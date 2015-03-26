c9a3d309154d41eb909d9abc380ab96f67814b5a
Cameron Spickert <cspickert@gmail.com>
2015-03-26 16:07:21 -0400

    Update config

--- config.fish
+++ config.fish
@@ -1,8 +1,17 @@
 #!/usr/bin/env fish
 
+# Editor
 set -x EDITOR (which emacs)
 
-set -x PATH /usr/local/arcanist/bin $PATH
-set -x PATH /usr/local/sbin $PATH
+# Path
+for d in /usr/local/arcanist/bin /usr/local/sbin
+    [ -d "$d" ]; and set -x PATH "$d" $PATH
+end
 
+# Python
+if [ -f ~/.config/pythonstartup.py ]
+   set -x PYTHONSTARTUP ~/.config/pythonstartup.py
+end
+
+# Aliases
 alias l ll
