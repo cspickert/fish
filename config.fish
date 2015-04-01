a065bc2533e70dd82797141cbffdb39f1e014606
Cameron Spickert <cspickert@gmail.com>
2015-04-01 17:14:46 -0400

    Improve config
    Inspried by: http://www.apertoire.net/switching-from-zsh-to-fish/

--- config.fish
+++ config.fish
@@ -1,12 +1,22 @@
 #!/usr/bin/env fish
 
-# Editor
-set -x EDITOR (which emacs)
-
 # Path
-for d in /usr/local/arcanist/bin /usr/local/sbin
-    [ -d "$d" ]; and set -x PATH "$d" $PATH
-end
+set -x PATH /usr/local/arcanist/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin /opt/X11/bin
+
+set -x TERM 'xterm-256color'
+
+# Enable color in grep
+set -x GREP_OPTIONS '--color=auto'
+set -x GREP_COLOR '3;33'
+
+set -x LESS '--ignore-case --raw-control-chars'
+set -x PAGER 'less'
+set -x EDITOR 'emacs'
+
+set -x LANG 'en_US.UTF-8'
+set -x LC_CTYPE 'en_US.UTF-8'
+set -x LC_MESSAGES 'en_US.UTF-8'
+set -x LC_COLLATE C
 
 # Python
 if [ -f ~/.config/pythonstartup.py ]
